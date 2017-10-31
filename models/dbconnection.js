a52361b692102bd60981eab31ebea30d89cc4961
Deepak <it.cloud33@gmail.com>
2017-10-31 10:40:34 +0530

    connection change

--- models/dbconnection.js
+++ models/dbconnection.js
@@ -1,10 +1,10 @@
 var mysql=require('mysql');
 var connection=mysql.createPool({
 
-    host: 'localhost',
-    user: 'deepak',
-    password: 'deepak',
-    database: 'cuberoot_portal_db'
+    host: '182.50.133.77:3306',
+    user: 'compaign',
+    password: 'compaign@123',
+    database: 'compaign_db'
 
 });
 module.exports=connection;
